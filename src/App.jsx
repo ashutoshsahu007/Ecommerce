b339f5df0349dd079077aef5edd35e63a505bae2
ashutosh sahu <ppaskumar@gmail.com>
2025-08-11 15:24:30 +0530

    routes protected feature implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Store from "./pages/Store";
 import About from "./pages/About";
 import ContactUs from "./pages/ContactUs";
 import Login from "./pages/Login";
+import AuthContext from "./store/auth-context";
+import { useContext } from "react";
 
 const App = () => {
+  const authCtx = useContext(AuthContext);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Store />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<ContactUs />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Store />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route
+        path="/"
+        element={authCtx.isLoggedIn ? <Store /> : <Navigate to="/login" />}
+      />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<ContactUs />} />
+      <Route
+        path="/login"
+        element={!authCtx.isLoggedIn ? <Login /> : <Navigate to="/" />}
+      />
+      <Route path="*" element={<Home />} />
+    </Routes>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { AuthContextProvider } from "./store/auth-context";
 import { CartContextProvider } from "./store/cart-context";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthContextProvider>
-      <CartContextProvider>
-        <App />
-      </CartContextProvider>
-    </AuthContextProvider>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <CartContextProvider>
+          <App />
+        </CartContextProvider>
+      </AuthContextProvider>
+    </BrowserRouter>
   </StrictMode>
 );
